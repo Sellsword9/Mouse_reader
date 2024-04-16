e9d74bb1b8f359f60e79ac5c2f78fd7ea551bb8e
Yeray Romero <yerayromerodev@gmail.com>
2024-04-16 23:01:41 +0200

    Refactor and storing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 extern crate hidapi;
 
 use hidapi::HidApi;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 
 fn main() {
     let api = HidApi::new().unwrap();
-    let vendor_id = 0x062a;
-    let product_id = 0x4102;
+    let vendor_id = 0x062a; // use lsusb to get the vendor_id and product_id
+    let product_id = 0x4102; // use lsusb to get the vendor_id and product_id
     let device = api.open(vendor_id, product_id).unwrap();
-    let report = [0x00, 0xff];
+    let report = [0x00, 0x01];
     let mut response: [u8; 8];
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("info.txt")
+        .unwrap();
     loop {
         device.write(&report).unwrap();
         response = [0u8; 8];
-        device.read_timeout(&mut response, 10000).unwrap(); // Aumentado a 3000 milisegundos
-        println!("Datos leídos: {:?}", response);
+        device.read_timeout(&mut response, 10000).unwrap();
+        println!("Data: {:?}", response);
+        if let Err(e) = writeln!(file, "Datos leídos: {:?}", response) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
     }
 }
